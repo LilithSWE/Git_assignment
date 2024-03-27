6996a86774c888364c6610b90cfbe32ed1784ad4
Amanda Hansson <amanda9307@gmail.com>
2024-03-27 13:15:30 +0100

    :bug: bugfixes calling json

--- js_modules/gallery.mjs
+++ js_modules/gallery.mjs
@@ -1,16 +1,21 @@
+import imageArray from "../images.json";
+
 export default function gallery() {
-
-  const main = document.getElementById('main');
-  const imageArray = require("../images")
-
+  const main = document.querySelector('main');
+  main.innerHTML = `
+  <h2>Gallery</h2>
+  <p>Here you will find my old, and current, hobby projects! 
+  I hope you like them! You can find more of these at my hobby instagram  
+  <a href="https://www.instagram.com/amandasminiatures/">@amandasminatures</a>. 
+  Hope to see you there as well!</p>
+  `
 
   imageArray.forEach(image => {
-
-    main.innerHTML = `
+    main.innerHTML += `
      <div class="item">
         <img src= ${image.scr} alt=${image.alt}>
         <div class="titleAndDateContainer">
-          <h2 class="headline">${image.title}</h2>
+          <h3 class="headline">${image.title}</h3>
           <p class="date">${image.date}</p>
         </div>
         <p class="imgtext">${image.imgText}</p>
