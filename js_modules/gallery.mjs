14412da53a3bd2744991740cbe8058f4e2385098
Amanda Hansson <amanda9307@gmail.com>
2024-03-27 13:15:30 +0100

    :bug: bugfixes calling json

--- js_modules/gallery.mjs
+++ js_modules/gallery.mjs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import imageArray from "../images.json";
 
 export default function gallery() {
@@ -8,28 +7,15 @@ export default function gallery() {
   <p>Here you will find my old, and current, hobby projects! 
   I hope you like them! You can find more of these at my hobby instagram  
   <a href="https://www.instagram.com/amandasminiatures/">@amandasminatures</a>. 
-  Hope to see you there as well!</p>`
+  Hope to see you there as well!</p>
+  `
+
   imageArray.forEach(image => {
     main.innerHTML += `
-     <div class="post">
-        <img src= ${image.scr} alt=${image.alt}>
-        <div class="titleAndDateContainer">
-          <h3 class="headline">${image.title}</h3>
-=======
-export default function gallery() {
-
-  const main = document.getElementById('main');
-  const imageArray = require("../images")
-
-
-  imageArray.forEach(image => {
-
-    main.innerHTML = `
      <div class="item">
         <img src= ${image.scr} alt=${image.alt}>
         <div class="titleAndDateContainer">
-          <h2 class="headline">${image.title}</h2>
->>>>>>> 5cdc25e (Add img file + html in JS for displaying images)
+          <h3 class="headline">${image.title}</h3>
           <p class="date">${image.date}</p>
         </div>
         <p class="imgtext">${image.imgText}</p>
