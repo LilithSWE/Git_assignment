13df3ce3416b31ec1911800f2df543a26e5bd893
Amanda Hansson <amanda9307@gmail.com>
2024-03-27 13:17:16 +0100

    cherry-pick merge between 2 branches + merge conflict fix

--- js_modules/header.mjs
+++ js_modules/header.mjs
@@ -1,3 +1,5 @@
+import homepage from "./homepage.mjs"
+import gallery from "./gallery.mjs"
 
 export default function header() {
 
@@ -10,6 +12,39 @@ export default function header() {
     <button id="galleryBtn">Gallery</button>
     <button id="productsBtn">Products</button>
     <button id="aboutBtn">About Me</button>
-  </div>
-  `
+  </div>`
+
+  addEventListerners()
+}
+
+function addEventListerners() {
+  const title = document.querySelector('h1')
+  const homeBtn = document.getElementById('homeBtn')
+  const galleryBtn = document.getElementById('galleryBtn')
+  const productsBtn = document.getElementById('productsBtn')
+  const aboutBtn = document.getElementById('aboutBtn')
+
+  title.addEventListener('click', () => {
+    homepage();
+    header();
+  })
+
+  homeBtn.addEventListener('click', () => {
+    homepage();
+    header();
+  })
+
+  galleryBtn.addEventListener('click', () => {
+    gallery()
+    header();
+  })
+
+  productsBtn.addEventListener('click', () => {
+    header();
+  })
+
+  aboutBtn.addEventListener('click', () => {
+    header();
+  })
+
 }
