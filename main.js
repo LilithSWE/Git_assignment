130d4e6c09ef8509f7f045bea715c75ff23cc9e1
Amanda Hansson <amanda9307@gmail.com>
2024-03-29 11:49:31 +0100

    display error for if js modules stop working for some reason.

--- main.js
+++ main.js
@@ -2,5 +2,10 @@ import './styling/style.css'
 import homepage from './js_modules/homepage.mjs';
 import header from './js_modules/header.mjs';
 
+const main = document.querySelector('main');
+main.innerHTML = `
+<h2>The JS module files are not connecting! Please contact your local software developer.</h2>`
+
+
 header();
 homepage();
