6bfdce5bad971ac71cd5261be3c10d100317fc7c
Amanda Hansson <amanda9307@gmail.com>
2024-03-29 11:34:52 +0100

    :lipstick: changed some styling and html for the gallery to make it more responsive

--- js_modules/gallery.mjs
+++ js_modules/gallery.mjs
@@ -1,15 +1,20 @@
 import imageArray from "../images.json";
 
-export default function gallery() {
+export default async function gallery() {
   const main = document.querySelector('main');
-  main.innerHTML = `
+  const gallery = document.createElement('div')
+  gallery.classList.add("gallery");
+
+  main.innerHTML = "";
+
+  gallery.innerHTML = `
   <h2>Gallery</h2>
   <p>Here you will find my old, and current, hobby projects! 
   I hope you like them! You can find more of these at my hobby instagram  
   <a href="https://www.instagram.com/amandasminiatures/">@amandasminatures</a>. 
   Hope to see you there as well!</p>`
   imageArray.forEach(image => {
-    main.innerHTML += `
+    gallery.innerHTML += `
      <div class="post">
         <img src= ${image.scr} alt=${image.alt}>
         <div class="titleAndDateContainer">
@@ -20,4 +25,6 @@ export default function gallery() {
       </div>
     `
   });
+  main.appendChild(gallery)
 }
+
