62c82e1676eafffd2919291fb19287881670f153
Amanda Hansson <amanda9307@gmail.com>
2024-03-29 11:36:23 +0100

    rebased about me onto main (+ tiny change to add commit msg)

--- js_modules/aboutMe.mjs
+++ js_modules/aboutMe.mjs
@@ -6,6 +6,5 @@ export default function aboutMe() {
   <p>My name is Amanda and I have been in the hobby for about a year and a half. 
   I wouldn't say I paint as regularly as I perhaps should, but I do enjoy painting with friends from time to time. 
   Recently I have joined up in a painting club in Gothenburg on Mondays, so I'm bound to increase the time spent painting going forward!</p>
-  <p> You can find more of my work at: <a href="https://www.instagram.com/amandasminiatures/">@amandasminatures</a>.</p>
-  `
+  <p> You can find more of my work at: <a href="https://www.instagram.com/amandasminiatures/">@amandasminatures</a>.</p>`
 }
